49877adc9024a24109acd541943cb08632374ab6
David Arevalo <davidarevaloc20@gmail.com>
2021-09-21 11:00:39 -0500

    Card Speaker

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './components/main/main.component';
 import { HomeRoutingModule } from './home-routing.module'
 
-
 @NgModule({
   declarations: [
     MainComponent
